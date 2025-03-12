fb25d99497d59b6aecf473092f1658c4882640b3
annita049 <e.annita2001@gmail.com>
2025-03-12 20:52:38 +0600

    commit final

--- src/services/search.service.js
+++ src/services/search.service.js
@@ -1,29 +1,38 @@
+import Fuse from "fuse.js";
 import User from "../models/user.model.js";
 
 const searchService = async (category, value) => {
     try {
-      let query = {};
+      const users = await User.find();
 
-      if (category === "company") {
-        query = {
-          jobExperience: { 
-            $elemMatch: { company: { $regex: value, $options: "i" } } 
-          }
-        };
-      }
-      else {
-        query = {
-          [category]: { $regex: value, $options: "i" }
-        };
-      }
+      const transformedUsers = users.map(user => ({
+        ...user._doc,
+        jobExperienceCompanies: user.jobExperience.map(job => job.company).join(" "), // Flatten company names
+      }));
 
-      const users = await User.find(query);
-      return users;
+      // Set up Fuse.js options
+      const fuseOptions = {
+        keys: [category === "company" ? "jobExperienceCompanies" : category],
+        threshold: 0.3,
+        distance: 100, // Consider matches within a distance
+        ignoreLocation: true, // Make search more flexible
+      };
+
+      const fuse = new Fuse(transformedUsers, fuseOptions);
+
+      const fuzzyResults = fuse.search(value).map(result => result.item);
+      return fuzzyResults;
     }
+
     catch (error) {
-        console.error("Search error:", error);
-        throw error;
+      console.error("Search error:", error);
+      throw error;
     }
 };
 
 export default searchService;
+
+
+
+
+
